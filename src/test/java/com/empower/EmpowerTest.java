f097b2cb6044ac3d58846ce5923539bd5ac47c76
Piyush Raj Bista <bista404>
2021-08-06 12:52:53 +0000

    Initial commit

---
+++ src/test/java/com/empower/EmpowerTest.java
@@ -0,0 +1,21 @@
+package com.empower;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+class EmpowerTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
